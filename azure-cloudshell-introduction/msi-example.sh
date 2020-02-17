c8f04aadd095ea4673e068f518b09f3918795db0
Adam Marczak <adam@marczak.io>
2020-02-17 17:04:27 +0100

    up

---
+++ azure-cloudshell-introduction/msi-example.sh
@@ -0,0 +1,9 @@
+# token
+
+response=$(curl http://localhost:50342/oauth2/token --data "resource=https://management.azure.com/" -H Metadata:true -s)
+access_token=$(echo $response | python -c 'import sys, json; print (json.load(sys.stdin)["access_token"])')
+echo The MSI access token is $access_token
+
+# call api
+
+az rest --uri https://graph.microsoft.com/v1.0/me/
