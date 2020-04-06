fa92cf3e2f600de4778bff23d6cbea16e6222d09
Adam Marczak <adam@marczak.io>
2020-04-06 23:31:33 +0200

    scripts for cloudshell intro video

---
+++ azure-cloudshell-introduction/script-examples.ps1
@@ -0,0 +1,19 @@
+Get-Date
+
+Get-AzVM -Status
+
+CD azure:\
+
+dir
+
+cd MySubscriptionName
+
+dir
+
+cd $HOME/clouddrive
+
+New-Item helloworld.ps1
+
+code ./helloworld.ps1
+
+Get-AzResourceGroup | select ResourceGroupName
--- azure-cloudshell-introduction/script-examples.sh
+++ azure-cloudshell-introduction/script-examples.sh
@@ -1,8 +1,22 @@
+#create VM
+
+az account list
+
+az group create --location westus --name MyRG
+
+az vm create -n myVM -g MyRG --image UbuntuLTS --generate-ssh-keys
+
+ssh username@ipaddress
+
+exit
+
+az group delete --name MyRG --no-wait
+
 # token
 
 response=$(curl http://localhost:50342/oauth2/token --data "resource=https://management.azure.com/" -H Metadata:true -s)
 access_token=$(echo $response | python -c 'import sys, json; print (json.load(sys.stdin)["access_token"])')
-echo The MSI access token is $access_token
+echo The access token is $access_token
 
 # call api
 
