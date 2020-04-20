61ada38db7a8238a7e5431fa5a8fb0c2c7ca86a0
Adam Marczak <adam@marczak.io>
2020-04-21 00:48:50 +0200

    updated script, added schema name

--- azure-data-factory-lookup-activity/02-create-metadata-table.sql
+++ azure-data-factory-lookup-activity/02-create-metadata-table.sql
@@ -1,9 +1,10 @@
 CREATE TABLE ExportedTables (
 	ID int identity(1,1) Primary Key,
+	Table_Schema varchar(100) not null,
 	Table_Name varchar(100) not null
 );
 GO
-INSERT INTO ExportedTables(Table_Name)
+INSERT INTO ExportedTables(Table_Schema, Table_Name)
 VALUES
-('Cars'),
-('Movies')
+('dbo','Cars'),
+('dbo','Movies')
