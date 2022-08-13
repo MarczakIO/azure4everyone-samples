c3e3220453c80a883758b43e9a01b6b709f2ecaa
Eytham Kishore <eytham.kishore@mphasis.com>
2022-08-13 12:07:39 +0530

    making changes to demo.py notebook

--- azure-databricks-introduction/demo1.py
+++ azure-databricks-introduction/demo1.py
@@ -4,7 +4,7 @@ blob_container_name = "citydatacontainer"
 blob_relative_path = "Safety/Release/city=Boston"
 blob_sas_token = r"?st=2019-02-26T02%3A34%3A32Z&se=2119-02-27T02%3A34%3A00Z&sp=rl&sv=2018-03-28&sr=c&sig=XlJVWA7fMXCSxCKqJm8psMOh0W4h7cSYO28coRqF2fs%3D"
 
-# COMMAND ----------
+
 
 wasbs_path = 'wasbs://%s@%s.blob.core.windows.net/%s' % (blob_container_name, blob_account_name, blob_relative_path)
 spark.conf.set('fs.azure.sas.%s.%s.blob.core.windows.net' % (blob_container_name, blob_account_name), blob_sas_token)
@@ -19,6 +19,7 @@ df.createOrReplaceTempView('source')
 # COMMAND ----------
 
 df.count()
+DataFrame
 
 # COMMAND ----------
 
